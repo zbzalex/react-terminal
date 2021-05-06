1f5ee2aec0440639bd665edf23c75112d6e2bcf4
Sasha <sasha.broslavskiy@gmail.com>
2021-05-06 12:43:24 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Terminal from "./Terminal";
 import "./App.css";
+import pkg from "../package.json";
 
 const App = () => (
   <Terminal
@@ -12,8 +13,16 @@ const App = () => (
             clear();
             break;
           case "help":
-            puts(`Hi, everyone!!!
-            \tHow are you?`);
+            puts(`Help:
+------------------------------
+Command         Description
+clear           To clear terminal
+help            To read help
+version         To see terminal current version
+------------------------------`);
+            break;
+          case "version":
+            puts(`Terminal version: ${pkg.version}`);
             break;
           default:
             puts(`${args[0]}: command not found`);
