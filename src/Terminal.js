36643c7668cfbd589f8ab86cfa36880d993500fa
Sasha <sasha.broslavskiy@gmail.com>
2021-05-06 12:34:06 +0300

    Update Terminal.js

--- src/Terminal.js
+++ src/Terminal.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 import "./Terminal.css";
 
 const parseText = (text) => {
-  text = text.replace(/\n/g, "<br />");
   text = text.replace(/\t/g, "&nbsp;&nbsp;");
   text = text.replace(/</g, "&lt;");
   text = text.replace(/>/g, "&gt;");
+
+  text = text.replace(/\n/g, "<br />");
   return text;
 };
 
